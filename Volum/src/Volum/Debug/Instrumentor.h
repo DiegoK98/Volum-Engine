814866101789bdd7c03100227603732c64cc4c27
DiegoK98 <diegsagdemig@hotmail.com>
2023-03-04 23:41:49 +0100

    Update Instrumentor.h
    - Removed __cdecl from scope name
    - Replaced " with ' in scope name

--- Volum/src/Volum/Debug/Instrumentor.h
+++ Volum/src/Volum/Debug/Instrumentor.h
@@ -76,14 +76,11 @@ namespace Volum {
 			if (m_profileCount++ > 0)
 				json << ",";
 
-			std::string name = result.Name;
-			std::replace(name.begin(), name.end(), '"', '\'');
-
 			json << std::setprecision(3) << std::fixed;
 			json << "{";
 			json << "\"cat\":\"function\",";
 			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
-			json << "\"name\":\"" << name << "\",";
+			json << "\"name\":\"" << result.Name << "\",";
 			json << "\"ph\":\"X\",";
 			json << "\"pid\":0,";
 			json << "\"tid\":" << result.ThreadID << ",";
@@ -163,6 +160,35 @@ namespace Volum {
 		std::chrono::time_point<std::chrono::steady_clock> m_startTimepoint;
 		bool m_stopped;
 	};
+
+	namespace InstrumentorUtils {
+
+		template <size_t N>
+		struct ChangeResult
+		{
+			char Data[N];
+		};
+
+		template <size_t N, size_t K>
+		constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+		{
+			ChangeResult<N> result = {};
+
+			size_t srcIndex = 0;
+			size_t dstIndex = 0;
+			while (srcIndex < N)
+			{
+				size_t matchIndex = 0;
+				while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+					matchIndex++;
+				if (matchIndex == K - 1)
+					srcIndex += matchIndex;
+				result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+				srcIndex++;
+			}
+			return result;
+		}
+	}
 }
 
 #define VLM_PROFILER 0
@@ -175,7 +201,7 @@ namespace Volum {
 		#define VLM_FUNC_SIG __PRETTY_FUNCTION__
 	#elif defined(__DMC__) && (__DMC__ >= 0x810)
 		#define VLM_FUNC_SIG __PRETTY_FUNCTION__
-	#elif defined(__FUNCSIG__)
+	#elif (defined(__FUNCSIG__) || (_MSC_VER))
 		#define VLM_FUNC_SIG __FUNCSIG__
 	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
 		#define VLM_FUNC_SIG __FUNCTION__
@@ -193,7 +219,8 @@ namespace Volum {
 #if VLM_PROFILER
 	#define VLM_PROFILE_BEGIN_SESSION(name, filepath) ::Volum::Instrumentor::Get().BeginSession(name, filepath)
 	#define VLM_PROFILE_END_SESSION() ::Volum::Instrumentor::Get().EndSession()
-	#define VLM_PROFILE_SCOPE(name) ::Volum::InstrumentationTimer timer##__LINE__(name);
+	#define VLM_PROFILE_SCOPE(name) constexpr auto fixedName = ::Volum::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+									::Volum::InstrumentationTimer timer##__LINE__(fixedName);
 	#define VLM_PROFILE_FUNCTION() VLM_PROFILE_SCOPE(VLM_FUNC_SIG)
 #else
 	#define VLM_PROFILE_BEGIN_SESSION(name, filepath)
@@ -205,7 +232,8 @@ namespace Volum {
 #if VLM_RENDERER_PROFILER
 	#define VLM_RENDERER_PROFILE_BEGIN_SESSION(name, filepath) ::Volum::Instrumentor::Get().BeginSession(name, filepath)
 	#define VLM_RENDERER_PROFILE_END_SESSION() ::Volum::Instrumentor::Get().EndSession()
-	#define VLM_RENDERER_PROFILE_SCOPE(name) ::Volum::InstrumentationTimer timer##__LINE__(name);
+	#define VLM_RENDERER_PROFILE_SCOPE(name) constexpr auto fixedName = ::Volum::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+											::Volum::InstrumentationTimer timer##__LINE__(fixedName);
 	#define VLM_RENDERER_PROFILE_FUNCTION() VLM_RENDERER_PROFILE_SCOPE(VLM_FUNC_SIG)
 #else
 	#define VLM_RENDERER_PROFILE_BEGIN_SESSION(name, filepath)
